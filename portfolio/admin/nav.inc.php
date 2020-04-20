8b305e30bde8ce992b1da20b59ae2f75136d5059
LaurenceM2015 <laurencemalonga@hotmail.com>
2020-04-20 16:50:10 +0100

    Edit connection and nav.inc file also add resume.html.

--- portfolio/admin/nav.inc.php
+++ portfolio/admin/nav.inc.php
@@ -5,8 +5,6 @@
             <a class="nav-link active" href="index.php">
               <span data-feather="home"></span>
               Dashboard <span class="sr-only">(current)</span>
-                
-             
             </a>
           </li>
           <li class="nav-item">
@@ -15,38 +13,29 @@
               All Posts
             </a>
           </li>
+          <?php 
+            if(isset($_SESSION['author_role'])){
+              if($_SESSION['author_role']=="admin"){
+                // Show all the link for admin
+                ?>
+                  <!-- ONLY ADMIN LINK HERE -->
+                  <li class="nav-item">
+                    <a class="nav-link" href="category.php">
+                      <span data-feather="file"></span>
+                      All Categories
+                    </a>
+                  </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file"></span>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart"></span>
-              Products
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
-              Customers
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
-            </a>
-          </li>
-        </ul>
+                   <!-- ONLY ADMIN LINK HERE -->
+                   <li class="nav-item">
+                    <a class="nav-link" href="page.php">
+                      <span data-feather="file"></span>
+                      All Pages
+                    </a>
+                  </li>
+
+            <?php } } ?>
+          </ul>
 
     
       </aside>
