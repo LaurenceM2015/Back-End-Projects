a5cb5440d11285d87b2ceb3d1128b4bd1b45dcec
LaurenceM2015 <laurencemalonga@hotmail.com>
2020-04-20 11:21:24 +0100

    Remove the dynamic navigation and replace it with a static secondary menu. Add a dynamic nav into a new file name main-nav.php add it as a template in the landing page.

--- portfolio/includes/nav.php
+++ portfolio/includes/nav.php
@@ -1,16 +1,39 @@
-<ul class="navbar-nav ml-auto">
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg text-uppercase fixed-top navbar-shrink" id="mainNav">
+    <div class="container-fluid">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Laurence Malonga</a>
+          
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+        <i class="fas fa-bars"></i>
+      </button>
 
-    <?php
-        foreach ($navItems as $item) {
-            echo 
-             "
-             <li class=\"nav-item mx-0 mx-lg-1\">
-             <a class=\"nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger\" href=\"$item[slug]\">$item[title]</a></li>
-             ";
-           
-        }
-    ?>
-</ul>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php">Home</a>
+            </li>
+        
+            <li class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger active" href="resume.html">Resume</a>
+          </li>
+
+          <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="resources.html">Resources</a>
+          </li>
+        
+          <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Resources</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- ./ Navbar --> 
 
 
 
